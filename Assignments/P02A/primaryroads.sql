22e66517b9d374b859e516123d24b4f5b032b2e8
Sahith98 <112120360+Sahith98@users.noreply.github.com>
2022-09-20 15:15:22 -0500

    Update primaryroads.sql

--- Assignments/P02A/primaryroads.sql
+++ Assignments/P02A/primaryroads.sql
@@ -1,1 +1,27 @@
+-- Table: public.primary_roads
 
+-- DROP TABLE IF EXISTS public.primary_roads;
+
+CREATE TABLE IF NOT EXISTS public.primary_roads
+(
+    gid integer NOT NULL DEFAULT nextval('primary_roads_gid_seq'::regclass),
+    linearid character varying(22) COLLATE pg_catalog."default",
+    fullname character varying(100) COLLATE pg_catalog."default",
+    rttyp character varying(1) COLLATE pg_catalog."default",
+    mtfcc character varying(5) COLLATE pg_catalog."default",
+    geom geometry(MultiLineString,4326),
+    CONSTRAINT primary_roads_pkey PRIMARY KEY (gid)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.primary_roads
+    OWNER to postgres;
+-- Index: indx_roads_geom
+
+-- DROP INDEX IF EXISTS public.indx_roads_geom;
+
+CREATE INDEX IF NOT EXISTS indx_roads_geom
+    ON public.primary_roads USING gist
+    (geom)
+    TABLESPACE pg_default;
